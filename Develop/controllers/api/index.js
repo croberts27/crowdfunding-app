9a33e182fb0c8b914464d466ccc4ed92ab6ac332
Sameer Mirza <srmirza14@gmail.com>
2023-06-28 11:15:18 -0400

    created profile model

--- Develop/controllers/api/index.js
+++ Develop/controllers/api/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const projectRoutes = require('./projectRoutes');
+const profileRoutes = require ('./profile-routes.js');
 
 router.use('/users', userRoutes);
 router.use('/projects', projectRoutes);
+router.use ('/profiles', profileRoutes);
 
 module.exports = router;
